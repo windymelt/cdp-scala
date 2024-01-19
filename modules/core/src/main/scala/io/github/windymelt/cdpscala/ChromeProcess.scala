a3b2b50ace1a7fd57d96f0a9a703e765d2a656de
Windymelt <windymelt@3qe.us>
2024-01-19 20:25:04 +0900

    split files

---
+++ modules/core/src/main/scala/io/github/windymelt/cdpscala/ChromeProcess.scala
@@ -0,0 +1,30 @@
+package io.github.windymelt.cdpscala
+
+import cats.effect.IO
+import cats.effect.Resource
+
+object ChromeProcess {
+  type ChromeProcessIO = Resource[IO, ChromeProcess]
+
+  val CHROME_SHELL = "chromium"
+
+  def spawn(): ChromeProcessIO =
+    Resource.make {
+      IO.delay(rawSpawnChrome())
+    } { proc =>
+      IO.delay(proc.destroy())
+    }
+
+  private def rawSpawnChrome(): ChromeProcess = ChromeProcess(
+    os
+      .proc(CHROME_SHELL, "--headless", "--remote-debugging-port=9222")
+      .spawn(stdout = os.Inherit, stderr = os.Inherit)
+  )("localhost", 9222)
+}
+
+case class ChromeProcess private (proc: os.SubProcess)(
+    host: String,
+    port: Int
+) {
+  def destroy(): Unit = proc.destroy()
+}
--- modules/core/src/main/scala/io/github/windymelt/cdpscala/Main.scala
+++ modules/core/src/main/scala/io/github/windymelt/cdpscala/Main.scala
@@ -36,31 +36,3 @@ object Main extends IOApp.Simple {
 }
 
 def msg = "Chrome DevTools Protocol wrapper for Scala test command"
-
-object ChromeProcess {
-  import cats.effect.*
-
-  type ChromeProcessIO = Resource[IO, ChromeProcess]
-
-  val CHROME_SHELL = "chromium"
-
-  def spawn(): ChromeProcessIO =
-    Resource.make {
-      IO.delay(rawSpawnChrome())
-    } { proc =>
-      IO.delay(proc.destroy())
-    }
-
-  private def rawSpawnChrome(): ChromeProcess = ChromeProcess(
-    os
-      .proc(CHROME_SHELL, "--headless", "--remote-debugging-port=9222")
-      .spawn(stdout = os.Inherit, stderr = os.Inherit)
-  )("localhost", 9222)
-}
-
-case class ChromeProcess private (proc: os.SubProcess)(
-    host: String,
-    port: Int
-) {
-  def destroy(): Unit = proc.destroy()
-}
